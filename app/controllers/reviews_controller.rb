73d9bccdaa8a85b28db9df26314fbb2000d2b882
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2024-11-27 15:47:35 -0600

    ejercicio dos Yield

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,18 @@
 class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.build(review_params)
-
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.'
+      redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render "restaurants/show", status: :unprocessable_entity
     end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
-
 end
